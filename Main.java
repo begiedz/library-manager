19342f5fd7c84775f90f3b036ce7b774b47e58e4
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2024-04-05 13:27:46 +0200

    add Book class

--- Main.java
+++ Main.java
@@ -1,4 +1,6 @@
-public class manager{
+import src.*;
+public class Main{
+
   public static void main (String[] args){
     System.out.println("Init commit"); }
 }
---
+++ src/Book.java
@@ -0,0 +1,16 @@
+package src;
+
+public class Book{
+
+  private String title;
+  private String author;
+  private int Year;
+  private String genre;
+  
+  public Book(String title, String author, int publishYear, String genre) {
+    this.title = title;
+    this.author = author;
+    this.Year = publishYear;
+    this.genre = genre;
+}
+}
