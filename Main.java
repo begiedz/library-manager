948ccce7175a0d10372520a51d4d1ae61ba3d387
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2024-05-08 19:11:55 +0200

    Add Java Scanner

--- Main.java
+++ Main.java
@@ -1,15 +1,45 @@
-import src.Book;
+import src.*;
+
+import java.util.Scanner;
 
 public class Main{
   public static void main (String[] args){
-    Book testBook = new Book("Title","Author", 2024, "Genre");
+    BookManager bookManager = new BookManager();
+    Scanner scanner = new Scanner(System.in);
+    boolean isRunning = false;
 
-    String str1 = "abc";
-    String str2 = "ABC";
-    
-    int result = str1.compareToIgnoreCase(str2);
-    
-    System.out.println(result);
-    System.out.println(testBook); 
+    while (isRunning) {
+      System.out.println("Menu:");
+      System.out.println("1. Show all books");
+      System.out.println("2. Add new book");
+      System.out.println("3. Edit book");
+      System.out.println("4. Delete book");
+      System.out.println("5. Exit program");
+      System.out.print("Choose action: ");
+    }
+    scanner.close();
+
+  if (!isRunning){
+    for (int i = 0; i < 1; i++) {
+       System.out.println("Menu:");
+      System.out.println("1. Show all books");
+      System.out.println("2. Add new book");
+      System.out.println("3. Edit book");
+      System.out.println("4. Delete book");
+      System.out.println("5. Exit program");
+      System.out.print("Choose action: ");
+    }
   }
+    
+      // Book testBook = new Book("Title","Author", 2024, "Genre");
+
+    // String str1 = "abc";
+    // String str2 = "ABC";
+    
+    // int result = str1.compareToIgnoreCase(str2);
+
+    // System.out.println(result);
+    // System.out.println(testBook); 
+  }
+
 }
