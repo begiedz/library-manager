dbe74bb59eca0ad61e97daf30473fdfca273f494
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2024-05-08 17:45:48 +0200

    add BookManager constructor

--- src/BookManager.java
+++ src/BookManager.java
@@ -1,11 +1,13 @@
 package src;
 
+import java.util.ArrayList;
+
 public class BookManager {
+  private ArrayList<Book> books;
 
   public BookManager(){
-   
+    this.books = new ArrayList<>();
   }
 
-  
 }
 
